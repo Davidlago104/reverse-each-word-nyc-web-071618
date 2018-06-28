5a2dba0b707351e3b6462f503d0a5a5f7daf0741
Davidlago104 <davidlago104@gmail.com>
2018-06-28 15:50:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   array = sentence1.split(" ")
-  new_array = array.collect do |word|
+  arr = array.collect do |word|
     word.reverse
   end
-  new_array.join(" ")
+  arr.join(" ")
 end  
